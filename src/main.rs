689e8d57926c52830eb30d8550f23594566a138c
clee9 <78444840+clee9@users.noreply.github.com>
2024-03-22 17:24:30 -0500

    implemented gui for connect 4. Few bugs still

--- src/main.rs
+++ src/main.rs
@@ -1,303 +1,374 @@
-use std::io;
+use notan::draw::*;
+use notan::math::{vec2, Mat3, Vec2};
+use notan::prelude::*;
 
-#[derive(Clone, PartialEq, Copy, Debug)]
+const WIDTH: f32 = 600.0;
+const HEIGHT: f32 = 600.0;
+const MARGIN: f32 = 50.0;
+
+
+#[derive(Default, Copy, Clone, PartialEq)]
 enum Player {
+    #[default]
     Red,
-    Black,
+    Yellow,
 }
 
-struct Move {
-    player: Player,
-    column: i32,
+enum Tied {
+    Yes,
 }
 
-impl Move {
-    // reads a move from a column string
-    fn read_move(c: String, player: &Player) -> Option<Move>{
-        let trimmed_c = c.trim().parse::<i32>(); //trim the newline character and parse the int
-
-        match trimmed_c{
-            // if int parsed, return the move
-            Ok(num) => {
-                if num > 0 && num <= 8{
-                    Some(Move{
-                        player: player.clone(),
-                        column: num,
-                    })
-                }else{
-                    println!("Enter a number within bounds (1-8)!");
-                    None
-                }
-            },
-
-            // return None
-            Err(_) => {
-                println!("Enter a number between 1 and 8!");
-                None
-            }
-        }
-    }
+#[derive(AppState)]
+struct State {
+    rng: Random,
+    font: Font,
+    turn: Player,
+    table: [[Option<Player>; 8];7],
+    winner: Option<Player>,
+    tie: Option<Tied>,
 }
 
-struct Board {
-    game_board: Vec<Vec<Option<Player>>>,
-}
+impl State {
+    fn new(gfx: &mut Graphics) -> Self {
+        let font = gfx
+            .create_font(include_bytes!("assets/Ubuntu-B.ttf"))
+            .unwrap();
 
-impl Board {
-    fn display(&self) -> (){ //prints out the board to the screen
-        for i in 0..6{ //iterate through rows
-            print!(" | ");
-            for j in 0..7{//iterate thru cols
-                match self.game_board[i][j] {
-                    Some(Player::Red) => print!("X | "),
-                    Some(Player::Black) => print!("O | "),
-                    None => print!("- | "),
-                }
-            }
-            println!("\n");
-        }
-    }
-    
-    fn new_board() -> Board {
-        Board { 
-            game_board: vec![vec![None; 7]; 6]
+        let mut rng = Random::default();
+        let turn = if rng.gen_bool(0.5) {
+            Player::Yellow
+        } else {
+            Player::Red
+        };
+
+        State {
+            rng,
+            font,
+            turn,
+            table: Default::default(),
+            winner: None,
+            tie: None,
         }
     }
 
-    fn update_board(&mut self, m: Move) {
+    fn reset(&mut self) {
+        self.turn = if self.rng.gen_bool(0.5) {
+            Player::Yellow
+        } else {
+            Player::Red
+        };
+
+        self.table = Default::default();
+        self.winner = None;
+        self.tie = None;
+    }
+}
+
+fn main() -> Result<(), String> {
+    let win = WindowConfig::default()
+        .set_multisampling(8)
+        .set_size(WIDTH as _, HEIGHT as _)
+        .set_vsync(true);
+
+    notan::init_with(State::new)
+        .add_config(win)
+        .add_config(DrawConfig)
+        .update(update)
+        .draw(draw)
+        .build()
+}
+
+fn update(app: &mut App, state: &mut State) {
+    if state.winner.is_some() {
+        if app.keyboard.was_pressed(KeyCode::Space) {
+            state.reset();
+        }
+        return;
+    }
+
+    let x = MARGIN;
+    let y = MARGIN;
+    let width = WIDTH - MARGIN * 2.0;
+    let height = HEIGHT - MARGIN * 2.0;
+
+    let tile_width = width / 7.0;
+    let tile_height = height / 6.0;
+
+    let (mx, my) = app.mouse.position();
+
+    if app.mouse.was_pressed(MouseButton::Left) {
+        // check bounds
+        if mx < x || mx > x + width {
+            return;
+        }
+
+        if my < y || my > y + height {
+            return;
+        }
+
+        // inside the table
+        let col = ((mx - x) / tile_width).floor();
+        let index = index_from_pos(col as _);
+
+        // set piece
+        let is_empty = matches!(state.table[index][6], None);
+        if !is_empty {
+            return;
+        }
+        
+        let mut row = 0;
+
         for i in (0..6).rev() {
-            // use m.column-1 because user inputs a num from 1-7, we need 0-6
-            let j: usize = usize::try_from(m.column - 1).unwrap(); 
-            if self.game_board[i][j] == None {
-                self.game_board[i][j] = Some(m.player);
+            if state.table[col as usize][i].is_none() {
+                row = i;
                 break;
             }
-        }
-    }
-    
-
-    fn is_full(&self) -> bool {
-        for j in 0..=6{//move horizontally
-            if self.game_board[0][j] == None { //go through the top row and find if any spot is open
-                return false
-            }
-        }
-        true
-    }
-    
-    
-    fn check_winner(&self) -> Option<Player> {
-        let mut winner: Option<Player> = None;
-        //horizontal check
+        }     
+            state.table[col as usize][row] = Some(state.turn);
         
-        for i in 0..=3{
-            for j in 0..=5{
-                if self.game_board[j][i]== self.game_board[j][i+1] && self.game_board[j][i]== self.game_board[j][i+2] && self.game_board[j][i]== self.game_board[j][i+3] {
-                    if self.game_board[j][i] != None {
-                        winner = self.game_board[j][i]
-                    }
-                }
-            }
+
+        // change turn
+        state.turn = match state.turn {
+            Player::Red => Player::Yellow,
+            Player::Yellow => Player::Red,
+        };
+
+        // game over
+
+        if let Some(winner) = check_winner(&state.table) {
+            state.winner = Some(winner);
+
+        } 
+        if is_full(&state.table) {
+            state.tie = Some(Tied::Yes);
         }
-    
-        //vertical check
-
-        for i in 0..=2{
-            for j in 0..=6{
-                if self.game_board[i][j]== self.game_board[i+1][j] && self.game_board[i][j]== self.game_board[i+2][j] && self.game_board[i][j]== self.game_board[i+3][j] {
-                    if self.game_board[i][j] != None {
-                        winner = self.game_board[i][j]
-                    }
-                }
-            }
-        }
-
-        //ascending diagonal check
-
-        for i in 3..=5{
-            for j in 0..=3{
-                if self.game_board[i][j]== self.game_board[i-1][j+1] && self.game_board[i][j]== self.game_board[i-2][j+2] && self.game_board[i][j]== self.game_board[i-3][j+3] {
-                    if self.game_board[i][j] != None {
-                        winner = self.game_board[i][j]
-                    }
-                }
-            }
-        }
-
-        //descending diagonal check
-
-        for i in 3..=5{
-            for j in 3..=6{
-                if self.game_board[i][j]== self.game_board[i-1][j-1] && self.game_board[i][j]== self.game_board[i-2][j-2] && self.game_board[i][j]== self.game_board[i-3][j-3] {
-                    if self.game_board[i][j] != None {
-                        winner = self.game_board[i][j]
-                    }
-                }
-            }
-        }
-        
-        winner
     }
-
 }
 
+fn is_full(table: &[[Option<Player>; 8]; 7]) -> bool {
+    for j in 0..=6{//move horizontally
+        if table[0][j] == None { //go through the top row and find if any spot is open
+            return false
+        }
+    }
+    true
+}
 
-fn main() {
-    // initialize a new game
-    let mut game = Board::new_board();
-    println!("Let's play Connect 4!\n");
-    game.display();
-    // playing the game
-    let mut current_player = Player::Red;
+fn check_winner(table: &[[Option<Player>; 8];7]) -> Option<Player> {
     let mut winner: Option<Player> = None;
+    //horizontal check
     
-    // start the main loop
-    while winner == None && !game.is_full() {
-        // repeatedly scan input until valid
-        let mut current_move: Option<Move> = None;
-        while current_move.is_none() {
-            match &current_player{
-                Player::Red => {println!("Input move for X: ")},
-                Player::Black => {println!("Input move for O: ")},
-            }    
-            let mut move_column = String::new();
-            io::stdin().read_line(&mut move_column).expect("Failed to read line");
-            current_move = Move::read_move(move_column, &current_player);
+    for i in 0..=3{
+        for j in 0..=5{
+            if table[j][i]== table[j][i+1] && table[j][i]== table[j][i+2] && table[j][i]== table[j][i+3] {
+                if table[j][i] != None {
+                    winner = table[j][i]
+                }
+            }
         }
-        //unwrap the optional
-        let current_move = current_move.unwrap();
-        
-        // update the board and display the board
-        game.update_board(current_move);
-        game.display();
-
-        // change the current player
-        current_player = match current_player{
-            Player::Red => Player::Black,
-            Player::Black => Player::Red,
-        };
-
-        // check to see if there is a winner
-        winner = game.check_winner();
-        // println!("{:?}",winner);
     }
 
-    // check to see if there is a winner or if there was a tie
-    match winner {
-        Some(Player::Red) => {println!("Winner is X!")},
-        Some(Player::Black) => {println!("Winner is O!")},
-        None=> {println!("Tie!")}
-    };   
+    //vertical check
+
+    for i in 0..=2{
+        for j in 0..=6{
+            if table[i][j]== table[i+1][j] && table[i][j]== table[i+2][j] && table[i][j]== table[i+3][j] {
+                if table[i][j] != None {
+                    winner = table[i][j]
+                }
+            }
+        }
+    }
+
+    //ascending diagonal check
+
+    for i in 3..=5{
+        for j in 0..=3{
+            if table[i][j]== table[i-1][j+1] && table[i][j]== table[i-2][j+2] && table[i][j]== table[i-3][j+3] {
+                if table[i][j] != None {
+                    winner = table[i][j]
+                }
+            }
+        }
+    }
+
+    //descending diagonal check
+
+    for i in 3..=5{
+        for j in 3..=6{
+            if table[i][j]== table[i-1][j-1] && table[i][j]== table[i-2][j-2] && table[i][j]== table[i-3][j-3] {
+                if table[i][j] != None {
+                    winner = table[i][j]
+                }
+            }
+        }
+    }
+    
+    winner
 }
 
+fn draw(gfx: &mut Graphics, state: &mut State) {
+    let mut draw = gfx.create_draw();
+    draw.clear(Color::new(0.68, 0.84, 0.90, 0.1));
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+    let x = MARGIN;
+    let y = MARGIN;
+    let width = WIDTH - MARGIN * 2.0;
+    let height = HEIGHT - MARGIN * 2.0;
 
-    #[test]
-    fn test_update_board() {
-        let mut my_board = Board::new_board();
-        let player_move = Move {
-            column: 3,  // Replace with the desired column value
-            player: Player::Red,  
-        };
-        let player_move2 = Move {
-            column: 3,  
-            player: Player::Black,  
-        };
+    let tile_width = width / 7.0;
+    let tile_height = height / 6.0;
 
-        my_board.update_board(player_move);
-        my_board.update_board(player_move2);
+    // draw "who is playing"
+    let size = vec2(tile_width, tile_height);
+    draw_text(
+        &mut draw,
+        &state.font,
+        size,
+        state.turn,
+        "Playing: ",
+        24.0,
+        vec2(300.0, MARGIN * 0.5),
+        0.2,
+    );
 
-        let expected_board = Board {
-            game_board: vec![
-                vec![None, None, None, None, None, None, None],
-                vec![None, None, None, None, None, None, None],
-                vec![None, None, None, None, None, None, None],
-                vec![None, None, None, None, None, None, None],
-                vec![None, None, Some(Player::Black), None, None, None, None],
-                vec![None, None, Some(Player::Red), None, None, None, None],
-            ],
-        };
-        assert_eq!(my_board.game_board, expected_board.game_board);
+    // drawing board
+    draw.rect((x, y), (width, height))
+        .stroke_color(Color::BLUE)
+        .stroke(6.0);
+
+    for index in 1..8 {
+            draw.line(
+                (x + tile_width * index as f32, y),
+                (x + tile_width * index as f32, y + height),
+            )
+            .width(2.0).color(Color::BLUE);
+            draw.line(
+                (x, y + tile_height * index as f32),
+                (x + width, y + tile_height * index as f32),
+            )
+            .width(2.0).color(Color::BLUE);
+       // }      
     }
 
-    #[test]
-    fn test_winner() {
-        let mut my_board = Board::new_board();
-        let player_move0 = Move {
-            column: 7,
-            player: Player::Red,  
-        };
-        let player_move1 = Move {
-            column: 6,
-            player: Player::Red,  
-        };
-        let player_move2 = Move {
-            column: 5,
-            player: Player::Red,  
-        };
-        let player_move3 = Move {
-            column: 4,
-            player: Player::Red,  
-        };
+    // // drawing pieces
+    state.table.iter().enumerate().for_each(|(i, p)| {
+        let pos_x = i as f32 * size.x + x*1.72;
 
-        let player_move10 = Move {
-            column: 7,
-            player: Player::Black,  
-        };
-        let player_move11 = Move {
-            column: 7,
-            player: Player::Black,  
-        };
-        let player_move12 = Move {
-            column: 7,
-            player: Player::Black,  
-        };
-        let player_move13 = Move {
-            column: 6,
-            player: Player::Black,  
-        };
+       for row_i in 0..7 {
 
-        let player_move22 = Move {
-            column: 6,
-            player: Player::Black,  
-        };
-        let player_move23 = Move {
-            column: 5,
-            player: Player::Black,  
-        };
+            let pos_y = row_i as f32 * size.y + y *1.8;
+            match p[row_i] {
+                Some(Player::Red) => draw_red(&mut draw, Vec2{x: size.x * 1.75, y: size.y * 1.75}, vec2(pos_x, pos_y)),
+                Some(Player::Yellow) =>  draw_yellow(&mut draw, Vec2{x: size.x * 1.75, y: size.y * 1.75}, vec2(pos_x, pos_y)),
+                None => {}
+            }
+        }
+    });
 
-        my_board.update_board(player_move10);
-        my_board.update_board(player_move11);
-        my_board.update_board(player_move12);
-        my_board.update_board(player_move13);
 
-        my_board.update_board(player_move22);
-        my_board.update_board(player_move23);
 
-        my_board.update_board(player_move0);
-        my_board.update_board(player_move1);
-        my_board.update_board(player_move2);
-        my_board.update_board(player_move3);
+    // draw final menu
+    if let Some(winner) = state.winner {
+        draw.rect((0.0, 0.0), (WIDTH, HEIGHT))
+            .color(Color::GRAY)
+            .alpha(0.8);
 
-        let expected_board = Board {
-            game_board: vec![
-                vec![None, None, None, None, None, None, None],
-                vec![None, None, None, None, None, None, None],
-                vec![None, None, None, None, None, None, Some(Player::Red)],
-                vec![None, None, None, None, None, Some(Player::Red), Some(Player::Black)],
-                vec![None, None, None, None, Some(Player::Red), Some(Player::Black), Some(Player::Black)],
-                vec![None, None, None, Some(Player::Red), Some(Player::Black), Some(Player::Black), Some(Player::Black)],
-            ],
-        };
+        let (text, x_offet) = ("Winner: ", size.x * 0.3);
 
-        let mut winner: Option<Player> = None;
+        draw_text(
+            &mut draw,
+            &state.font,
+            size,
+            winner,
+            text,
+            48.0,
+            vec2(WIDTH * 0.5 - x_offet, HEIGHT * 0.5),
+            0.6,
+        );
 
-        winner = my_board.check_winner();
-
-        assert_eq!(my_board.game_board, expected_board.game_board);
-        assert_eq!(expected_board.check_winner(), winner);
+        draw.text(&state.font, "Press SPACE to reset")
+            .position(WIDTH * 0.5, HEIGHT * 0.75)
+            .size(32.0)
+            .h_align_center()
+            .v_align_middle()
+            .color(Color::BLACK);
     }
+
+    if let Some(_tie) = &state.tie {
+        draw.rect((0.0, 0.0), (WIDTH, HEIGHT))
+            .color(Color::GRAY)
+            .alpha(0.8);
+
+
+        draw.text(&state.font, "Tie")
+            .position(WIDTH * 0.5, HEIGHT * 0.5)
+            .size(48.0)
+            .h_align_center()
+            .v_align_middle()
+            .color(Color::BLACK);
+
+
+        draw.text(&state.font, "Press SPACE to reset")
+            .position(WIDTH * 0.5, HEIGHT * 0.75)
+            .size(32.0)
+            .h_align_center()
+            .v_align_middle()
+            .color(Color::BLACK);
+    }
+
+    gfx.render(&draw);
+}
+
+#[allow(clippy::too_many_arguments)]
+fn draw_text(
+    draw: &mut Draw,
+    font: &Font,
+    size: Vec2,
+    player: Player,
+    text: &str,
+    font_size: f32,
+    pos: Vec2,
+    scale: f32,
+) {
+    // drawing text
+    draw.text(font, text)
+        .color(Color::BLACK)
+        .size(font_size)
+        .v_align_middle()
+        .h_align_center()
+        .position(pos.x, pos.y);
+
+    let bounds = draw.last_text_bounds();
+
+    let pos = vec2(bounds.max_x() + 30.0, bounds.center_y());
+    let mm = Mat3::from_translation(pos)
+        * Mat3::from_scale(Vec2::splat(scale))
+        * Mat3::from_translation(-pos);
+    draw.transform().push(mm);
+    match player {
+        Player::Red => draw_red(draw, size, pos),
+        Player::Yellow => draw_yellow(draw, size, pos),
+    }
+    draw.transform().pop();
+}
+
+fn draw_red(draw: &mut Draw, size: Vec2, pos: Vec2) {
+    let radius = size.x/4.0 ;
+    draw.circle(radius)
+        .position(pos.x, pos.y)
+        .fill_color(Color::RED)
+        .fill();
+}
+
+fn draw_yellow(draw: &mut Draw, size: Vec2, pos: Vec2) {
+    let radius = size.x/4.0;
+    draw.circle(radius)
+        .position(pos.x, pos.y)
+        .fill_color(Color::YELLOW)
+        .fill();
+}
+
+fn index_from_pos(x: usize) -> usize {
+    x
 }
